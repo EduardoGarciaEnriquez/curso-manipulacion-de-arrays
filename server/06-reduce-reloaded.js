ee47737a9e0cf32091cf62ce8046b379959111e5
Eduardo García <garciaenriquez.carloseduardo@gmail.com>
2023-01-13 04:10:56 +0000

    fix: 06 reduce reloared added return

--- server/06-reduce-reloaded.js
+++ server/06-reduce-reloaded.js
@@ -1,11 +1,12 @@
 const numbers = [1, 2, 3, 4];
 
-const newObj = numbers.reduce((obj, item) => {
+const newObj = numbers.reduce((obj, element) => {
     if(!obj[element]){
         obj[element] = 1;
     }else{
-        obj[element] += 1;
+        obj[element] = obj[element] + 1;
     }
+    return obj
 }, {})
 
 console.log(newObj);
