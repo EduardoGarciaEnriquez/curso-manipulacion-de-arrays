2a5a86e4071b79f4414ccba91eba1153334652fc
Eduardo García <garciaenriquez.carloseduardo@gmail.com>
2023-01-13 13:24:19 +0000

    12 concat

---
+++ server/12-concat.js
@@ -0,0 +1,5 @@
+const landAnimals = ['elephant', 'jiraff', 'lion', 'tiger', 'polar'];
+const waterAnimals = ['wale', 'dolphin', 'crab', 'shrimp'];
+
+const allAnimals = landAnimals.concat(waterAnimals);
+console.log(allAnimals);
